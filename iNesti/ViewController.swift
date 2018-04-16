7d40061ba2f02007622a904df6a2dc5ff05ef632
cunqi <cx363@nyu.edu>
2018-04-16 15:20:26 -0400

    Initial Commit

---
+++ iNesti/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iNesti
+//
+//  Created by Cunqi Xiao on 4/16/18.
+//  Copyright © 2018 iShareLLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
