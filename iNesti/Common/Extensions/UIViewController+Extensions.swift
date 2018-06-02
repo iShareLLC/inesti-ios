d3491ec570d547403e2cdd2352f4667f34a53914
Zian Chen <doomon@gmail.com>
2018-06-02 03:32:14 -0400

    Adding email, phone, cop

--- iNesti/Common/Extensions/UIViewController+Extensions.swift
+++ iNesti/Common/Extensions/UIViewController+Extensions.swift
@@ -10,6 +10,15 @@ import UIKit
 
 extension UIViewController {
     
+    func displayAlert(title: String, message: String, completion:((UIAlertActionStyle) -> Void)?) {
+        let v = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "好", style: .default) { (action) in
+            completion?(action.style)
+        }
+        v.addAction(okAction)
+        present(v, animated: true, completion: nil)
+    }
+    
     func displayAlertOkCancel(title: String, message: String, completion:((UIAlertActionStyle) -> Void)?) {
         let v = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let cancelAction = UIAlertAction(title: "取消", style: .cancel) { (action) in
--- iNesti/Features/Detail/RentalContactViewController.swift
+++ iNesti/Features/Detail/RentalContactViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Messages
+import MessageUI
 
 protocol RentalContactViewControllerDelegate: class {
     func handleDismiss()
@@ -15,7 +17,42 @@ protocol RentalContactViewControllerDelegate: class {
 class RentalContactViewController: UIViewController {
 
     @IBOutlet weak var contactViewContainer: UIView!
+    @IBOutlet weak var phoneButton: UIButton!
+    @IBOutlet weak var emailButton: UIButton!
+    @IBOutlet weak var copyButton: UIButton!
+    
     weak var delegate: RentalContactViewControllerDelegate?
+    let contact = RentalContact(phone: "(718)566-3380", wechat: "zianabc1234", email: "doomon@gmail.com")
+    
+    //MARK: Action Handler
+    @IBAction func handleCopyButton(sender: UIButton) {
+        UIPasteboard.general.string = contact.wechat
+        self.displayAlert(title: "复制微信号", message: "复制\"\(contact.wechat)\"至黏贴簿", completion: nil)
+    }
+    
+    @IBAction func handlePhoneButton(sender: UIButton) {
+        if let url = URL(string: "tel://\(contact.phone)"), UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url)
+        } else {
+            DLog("Wrong phone number")
+        }
+    }
+    
+    @IBAction func handleEmailButton(sender: UIButton) {
+        
+        if MFMailComposeViewController.canSendMail() {
+            let composeVC = MFMailComposeViewController()
+            composeVC.mailComposeDelegate = self
+            
+            // Configure the fields of the interface.
+            composeVC.setToRecipients([contact.email])
+            composeVC.setSubject("iNesti Rental")
+            composeVC.setMessageBody("Hey I am interested at your rental listing!", isHTML: false)
+            
+            // Present the view controller modally.
+            self.present(composeVC, animated: true, completion: nil)
+        }
+    }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let firstTouch = touches.first {
@@ -25,4 +62,17 @@ class RentalContactViewController: UIViewController {
             }
         }
     }
+}
+
+extension RentalContactViewController: MFMailComposeViewControllerDelegate {
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+struct RentalContact {
+    let phone: String
+    let wechat: String
+    let email: String
 }
